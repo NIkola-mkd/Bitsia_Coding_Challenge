47ecc9d771cabe4e714fc116f83e64bbd8433115
Nikola-mkd <nikolad150@gmail.com>
2022-03-01 21:51:07 +0100

    invoice design

---
+++ public/js/addRow.js
@@ -0,0 +1,15 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!********************************!*\
+  !*** ./resources/js/addRow.js ***!
+  \********************************/
+$(function () {
+  var cnt = 0;
+  $("#add").on("click", function () {
+    event.preventDefault();
+    cnt++;
+    $("#tableBody").append("\n            <tr class=\"bg-white\">\n             <td>" + cnt + "</td>\n                  <td class=\"col-2\"><input class=\"form-control\" type=\"text\" id=\"product_name\" name=\"product_name[]\"></td>\n                   <td class=\"col-2\"><textarea class=\"form-control \" type=\"text\" id=\"description\" name=\"description[]\">\n                  </textarea>\n                  </td>\n                  <td class=\"col-1\"><input class=\"form-control\" type=\"number\" id=\"due\" name=\"quantity[]\"></td>\n                  <td class=\"col-1\"><input class=\"form-control\" type=\"number\" id=\"due\" name=\"unit_price[]\"> <span>cent</span></td>\n                 <td class=\"col-2\">123</td>\n                 <td class=\"col-2\"><i class=\"fa fa-trash-o\"></i></td>\n                 </tr>\n        ");
+  });
+});
+/******/ })()
+;
